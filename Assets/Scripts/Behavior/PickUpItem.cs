0be356c90f1bdcf158b101a016d6865cf6bb7112
Jedenxct <tabara.jan@seznam.cz>
2018-02-06 20:57:38 +0100

    fixed the item respawning bug when changing scenes

--- Assets/Scripts/Behavior/PickUpItem.cs
+++ Assets/Scripts/Behavior/PickUpItem.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using Assets.Scripts.Model;
 using System;
+using System.Collections.Generic;
 
 namespace Assets.Scripts.Controller
 {
@@ -9,6 +10,7 @@ namespace Assets.Scripts.Controller
         private Inventory _inventory;
         private ItemRepository _itemRepository;
 
+        public static List<string> _deletedItemsNames = new List<string>();
         private Item _item;
         private bool _showMessage;
 
@@ -38,6 +40,7 @@ namespace Assets.Scripts.Controller
                 if (Input.GetKeyDown(KeyCode.Return))
                 {
                     _inventory.AddItem(_item.Id);
+                    _deletedItemsNames.Add(gameObject.name);
                     Destroy(gameObject);
                 }
             }
@@ -55,6 +58,22 @@ namespace Assets.Scripts.Controller
                 var message = String.Format("Pick up: {0}. (enter)", _item.Name);
                 GUI.Label(new Rect(Screen.width / 2 + 50, Screen.height / 2, 150, 40), message);
             }
+        }
+
+        /// <summary>
+        /// This method will delete all gameobjects, that are marked deleted from e.g. previous scenes
+        /// </summary>
+        private void DisposePreviouslyDeleted()
+        {
+            for (int i = 0; i < _deletedItemsNames.Count; i++)
+            {
+                Destroy(GameObject.Find(_deletedItemsNames[i]));
+            }
+        }
+
+        private void OnLevelWasLoaded(int level)
+        {
+        DisposePreviouslyDeleted();
         }
     }
 }
