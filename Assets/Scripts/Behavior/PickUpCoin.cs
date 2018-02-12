6937eecec49875eb32b8d2bceda5bd36e32c26fa
Elevxnth <tabara.jan@seznam.cz>
2018-02-12 23:06:22 +0100

    player can now pick up coins

---
+++ Assets/Scripts/Behavior/PickUpCoin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCoin : MonoBehaviour {
+
+    private PlayerBalance _playerBalance;
+    private int _value;
+
+    public static List<string> _deletedItemsNames = new List<string>();
+
+    void Start () {
+        _playerBalance = GameObject.FindGameObjectWithTag("Balance").GetComponent<PlayerBalance>();
+        _value = new System.Random().Next(1, 11);
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _playerBalance.Balance += _value;
+        Destroy(gameObject);
+    }
+}
