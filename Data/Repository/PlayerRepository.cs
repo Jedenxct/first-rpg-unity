131cbc00d43f7e1339eccd10961836039cc98218
Elevxnth <tabara.jan@seznam.cz>
2018-02-28 21:47:55 +0100

    add database and a repository to operate on it

---
+++ Data/Repository/PlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Data.DAO;
+
+namespace Data.Repository
+{
+    public class PlayerRepository
+    {
+        public async Task InsertPlayer(Player player)
+        {
+            using (var context = DatabaseContext.Get())
+            {
+                context.Players.Add(player);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
